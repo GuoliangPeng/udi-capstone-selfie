ea6c58f58d437b5e30e1e70e8c44505cf51e807b
student <nikhil.nakhate@gmail.com>
2019-10-07 17:48:50 -0700

    Correction

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,31 +1,10 @@
 from styx_msgs.msg import TrafficLight
-
-class TLClassifier(object):
-    def __init__(self):
-        #TODO load classifier
-        pass
-
-    def get_classification(self, image):
-        """Determines the color of the traffic light in the image
-
-        Args:
-            image (cv::Mat): image containing the traffic light
-
-        Returns:
-            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
-
-        """
-        #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
-
 import tensorflow as tf
 from PIL import Image
 import cv2
 import numpy as np
 import os
 from matplotlib.pyplot import imshow
-import matplotlib.pyplot as plt
-%matplotlib inline
 
 class TLClassifier(object):
     def __init__(self):
